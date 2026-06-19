457f6c3eb7e8c092b6ea027f6437e8091d76f766
Steven Crawford <steven.crawford.uk@gmail.com>
2026-06-19 20:44:17 +0200

    chore: Add dual copyright attribution
    - Add Steven Crawford copyright alongside original Ken'ichiro Oyama
      copyright in LICENSE and main.go header

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 /*
 Copyright © 2026 Ken'ichiro Oyama <k1lowxb@gmail.com>
+Copyright © 2026 Steven Crawford
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
